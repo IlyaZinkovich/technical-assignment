4d967a47fae01e5e2143ed3d5d3f832bce8d59dd
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2017-09-08 17:41:55 +0300

    Added RoutesGraph initialization logic

--- src/main/scala/route/RoutesGraph.scala
+++ src/main/scala/route/RoutesGraph.scala
@@ -4,8 +4,20 @@ import route.figure.Figure
 
 class RoutesGraph(width: Int, height: Int, figure: Figure) {
 
-	val adjacencyMap: Map[(Int, Int), Set[(Int, Int)]] = {
-		Map((0, 0) -> Set((0, 3)))
-		figure.moves()
+	private val adjacencyMap: Map[(Int, Int), Set[(Int, Int)]] = {
+		val vertices = for(x <- List.range(0, height); y <- List.range(0, width)) yield (x, y)
+		vertices.map(fromVertex => fromVertex -> adjacentVertices(vertices, fromVertex)).toMap
+	}
+
+	private def adjacentVertices(vertices: List[(Int, Int)], fromVertex: (Int, Int)) = {
+		vertices.map(toVertex => (toVertex, canMove(fromVertex, toVertex))).filter(_._2).map(_._1).toSet
+	}
+
+	private def canMove(fromVertex: (Int, Int), toVertex: (Int, Int)) = {
+		figure.moves().map(move => move.apply(fromVertex, toVertex)).reduce(_ | _)
+	}
+
+	def route(initialPosition: (Int, Int)): Seq[(Int, Int)] = {
+		List()
 	}
 }
--- src/test/scala/route/figure/PawnSpec.scala
+++ src/test/scala/route/figure/PawnSpec.scala
@@ -4,40 +4,38 @@ import org.scalatest.WordSpecLike
 
 class PawnSpec extends WordSpecLike {
 
-	val pawn = new Pawn()
-
 	"A Pawn" should {
 
 		"move up" in {
-			assert(pawn.canMoveUp((3, 0), (0, 0)))
+			assert(Pawn.canMoveUp((3, 0), (0, 0)))
 		}
 
 		"move down" in {
-			assert(pawn.canMoveDown((0, 0), (3, 0)))
+			assert(Pawn.canMoveDown((0, 0), (3, 0)))
 		}
 
 		"move left" in {
-			assert(pawn.canMoveLeft((0, 3), (0, 0)))
+			assert(Pawn.canMoveLeft((0, 3), (0, 0)))
 		}
 
 		"move right" in {
-			assert(pawn.canMoveRight((0, 0), (0, 3)))
+			assert(Pawn.canMoveRight((0, 0), (0, 3)))
 		}
 
 		"move up left" in {
-			assert(pawn.canMoveUpLeft((2, 2), (0, 0)))
+			assert(Pawn.canMoveUpLeft((2, 2), (0, 0)))
 		}
 
 		"move up right" in {
-			assert(pawn.canMoveUpRight((2, 0), (0, 2)))
+			assert(Pawn.canMoveUpRight((2, 0), (0, 2)))
 		}
 
 		"move down left" in {
-			assert(pawn.canMoveDownLeft((0, 2), (2, 0)))
+			assert(Pawn.canMoveDownLeft((0, 2), (2, 0)))
 		}
 
 		"move down right" in {
-			assert(pawn.canMoveDownRight((0, 0), (2, 2)))
+			assert(Pawn.canMoveDownRight((0, 0), (2, 2)))
 		}
 
 	}
